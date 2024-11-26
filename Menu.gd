2fe0264c5641970894aced8090eee9c60a78aeae
EmanuelPimenta <pimentaemanuel0701@gmail.com>
2024-11-26 14:53:13 -0300

    alterações

--- Menu.gd
+++ Menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Main.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://controlesinstrucoes.tscn")
